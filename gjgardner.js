98188d094e43e275ca37b68e6058194712d0f022
Douglas Bamber <douglasbamber@gmail.com>
2019-01-08 14:06:51 +1300

    ADD mike-greer-homes.js webtask

--- gjgardner.js
+++ gjgardner.js
@@ -13,6 +13,25 @@ module.exports = function (cb) {
       const str = $('script:not([src])')[4].children[0].data;
       // HACK
       const listings = JSON.parse(str.match(/(?<=window.gjgardner.listings = )(.*)(?=;)/)[0]);
+      // "Title": "Stafford",
+      // "Bedrooms": "5",
+      // "Bathrooms": "4",
+      // "Garages": "4",
+      // "LivingAreas": "3",
+      // "Storeys": "1",
+      // "FloorArea": "484.1",
+      // "PorchArea": "5.60",
+      // "TotalArea": "489.70",
+      // "CarportArea": "0.00",
+      // "PatioArea": "0.00",
+      // "Width": "22.30",
+      // "Length": "41.00",
+      // "AlfrescoArea": "0.00",
+      // "ID": 236,
+      // "Link": "/english/home-designs/stafford/",
+      // "ThumbnailImage": "https://www.gjgardner.co.nz/assets/Uploads/home-design/thumbnail/Estate-Stafford-HouseDesign-Render-Thumbnail.jpg",
+      // "CollectionTitle": "Estate Collection",
+      // "Price": 746467.7777777778,
       cb(null, listings);
     } else {
       cb(error);
--- mike-greer-homes.js
+++ mike-greer-homes.js
@@ -1,1 +1,38 @@
-module.exports = function (cb) {cb(null, 'Hello');}
+// const request = require('request@2.81.0');
+// const cheerio = require('cheerio@0.19.0');
+const request = require('request');
+const cheerio = require('cheerio');
+
+module.exports = function (cb) {
+  const urls = [
+    'https://www.mikegreerhomes.co.nz/design-and-build/entry',
+    'https://www.mikegreerhomes.co.nz/design-and-build/transitional',
+    'https://www.mikegreerhomes.co.nz/design-and-build/family'
+  ];
+  const url = 'https://www.mikegreerhomes.co.nz/design-and-build/entry';
+
+  request({ url, rejectUnauthorized: false }, function (error, response, html) {
+    if (!error) {
+      const $ = cheerio.load(html);
+      const json = [];
+      $('.packageset').children().each(function(i, elem) {
+        const data = $(this);
+        const title = data.find('.main').children().text().trim();
+        const bedrooms = data.find('.bedroom').text().trim();
+        const bathrooms = data.find('.bathroom').text().trim();
+        const garages = data.find('.garage').text().trim();
+        const id = data.attr('id').replace('property_detail_', '');
+        json[i - 1] = {
+          title,
+          bedrooms: Number(bedrooms),
+          bathrooms: Number(bathrooms),
+          garages: Number(garages),
+          id: Number(id)
+        }
+      });
+      cb(null, json);
+    } else {
+      cb(error);
+    }
+  })
+}
