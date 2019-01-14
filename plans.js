29d1657bfdcb4bcf5a130875a08ff9e04ed36696
Douglas Bamber <douglasbamber@gmail.com>
2019-01-15 12:07:24 +1300

    ADD scaffold out plans endpoint and to post and get events

--- plans.js
+++ plans.js
@@ -1,11 +1,37 @@
-const express    = require('express');
-const Webtask    = require('webtask-tools');
+const express = require('express');
+const Webtask = require('webtask-tools');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
 
+app.post('/', (req, res) => {
+  const plansMap = req.body.reduce((acc, cur) => {
+    if (!cur.uuid) return acc;
+    acc[cur.uuid] = cur;
+    return acc;
+  }, {})
+  req.webtaskContext.storage.get((error) => {
+    if (error) return res.send(error);
+    req.webtaskContext.storage.set(plansMap, (error) => {
+      if (error) return res.send(error);
+      res.sendStatus(200);
+    });
+  });
+});
+
 app.get('/', (req, res) => {
+  req.webtaskContext.storage.get((error, data = {}) => {
+    if (error) return res.send(error);
+    return res.send(Object.values(data));
+  });
+});
+
+app.patch('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete('/', (req, res) => {
   res.sendStatus(200);
 });
 
